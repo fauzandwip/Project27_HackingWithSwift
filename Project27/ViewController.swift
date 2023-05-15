035369016534e5235ed550c6825ff7d367962116
fauzandp <fauzan27dp@gmail.com>
2023-05-15 16:40:16 +0700

    Project27 | Core Graphics add code to drawCircle and drawCheckerBoard

--- Project27/ViewController.swift
+++ Project27/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerBoard()
         default:
             break
         }
@@ -50,5 +54,41 @@ class ViewController: UIViewController {
         
         imageView.image = image
     }
+    
+    func drawCircle() {
+            let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+            
+            let image = renderer.image { ctx in
+                let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+                
+                ctx.cgContext.setFillColor(UIColor.red.cgColor)
+                ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+                ctx.cgContext.setLineWidth(10)
+                
+                ctx.cgContext.addEllipse(in: rectangle)
+                ctx.cgContext.drawPath(using: .fillStroke)
+            }
+            
+            imageView.image = image
+        }
+        
+        func drawCheckerBoard() {
+            let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+            
+            let image = renderer.image { ctx in
+                ctx.cgContext.setFillColor(UIColor.black.cgColor)
+                
+                for row in 0..<8 {
+                    for col in 0..<8 {
+                        if (row + col) % 2 == 0 {
+                            ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                        }
+                    }
+                }
+            }
+            
+            imageView.image = image
+        }
+    
 }
 
